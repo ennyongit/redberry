f1d92e69b9d7c375d2a40066048b7777595e5dd6
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-08-16 15:13:19 +0400

    Add TypeScript to the Project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full bg-amber-600">
+        <h1 className="text-red-600">this project works</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
